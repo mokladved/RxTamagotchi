fc80ad2fc38c404a72017bd761abddfc6f8e6fce
moklad <dev.moklad@gmail.com>
2025-08-23 17:09:00 +0900

    feat: 메인화면 기본 hierarchy 설정

--- Tamagotchi/Views/HomeViewController.swift
+++ Tamagotchi/Views/HomeViewController.swift
@@ -7,14 +7,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class HomeViewController: BaseViewController {
+    let chatImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let chatLabel = {
+        let label = UILabel()
+        label.text = "테스트"
+        return label
+    }()
+    
+    let stateWrappedView = UIView()
+    
+    let tamagotchiImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let nameLabel = {
+        let label = UILabel()
+        label.text = "테스트"
+        return label
+    }()
+    
+    let stateLabel = {
+        let label = UILabel()
+        label.text = "테스트"
+        return label
+    }()
+    
+    let riceTextField = {
+        let tf = UITextField()
+        return tf
+    }()
+    
+    let riceButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    let waterTextField = {
+        let tf = UITextField()
+        return tf
+    }()
+    
+    let waterButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    override func configureHierarchy() {
+        view.addSubview(chatImageView)
+        chatImageView.addSubview(chatLabel)
+        
+        view.addSubview(stateWrappedView)
+        stateWrappedView.addSubview(tamagotchiImageView)
+        stateWrappedView.addSubview(nameLabel)
+        stateWrappedView.addSubview(stateLabel)
+        
+        view.addSubview(riceTextField)
+        view.addSubview(riceButton)
+        view.addSubview(waterTextField)
+        view.addSubview(waterButton)
+        
     }
-
-
+    
+    override func configureLayout() {
+        
+    }
 }
 
-    
+
+
