8110a6f2801e8aac8c090d2074645e9537664a51
moklad <dev.moklad@gmail.com>
2025-08-23 15:18:36 +0900

    feat: 프로젝트 추가 및 설정
    - .gitignore 설정
    - RxSwift, RxCoCoa, SnapKit 패키지 추가
    - 코드베이스 기반으로 구현하기 위한 기본 설정

---
+++ Tamagotchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by Youngjun Kim on 8/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+    
