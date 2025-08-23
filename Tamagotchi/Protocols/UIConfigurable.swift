8307e6a8b6c163e4eee074c6b9205b3ceddba7a7
moklad <dev.moklad@gmail.com>
2025-08-23 16:14:47 +0900

    feat: 구조적인 UI 구현을 위한 UIConfigurable protocol 생성

---
+++ Tamagotchi/Protocols/UIConfigurable.swift
@@ -0,0 +1,14 @@
+//
+//  UIConfigurable.swift
+//  Tamagotchi
+//
+//  Created by Youngjun Kim on 8/23/25.
+//
+
+import Foundation
+
+protocol UIConfigurable {
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}
