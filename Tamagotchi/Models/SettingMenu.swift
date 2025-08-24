47120b6f4f29ad78d40a1d89c66d90482d4534e1
moklad <dev.moklad@gmail.com>
2025-08-24 22:08:02 +0900

    feat: 설정 메뉴 구현을 위한 구조체 생성

---
+++ Tamagotchi/Models/SettingMenu.swift
@@ -0,0 +1,13 @@
+//
+//  SettingMenus.swift
+//  Tamagotchi
+//
+//  Created by Youngjun Kim on 8/24/25.
+//
+
+import Foundation
+
+struct SettingMenu {
+    let title: String
+    let imageName: String
+}
