b1cfacd716a822aca05a9af1b761bafa83c70457
moklad <dev.moklad@gmail.com>
2025-08-25 04:31:52 +0900

    fix: 조건에 따라 올바른 VC가 보이도록 로직 수정
    - 다마고치 선택이 올바르게 이루어졌는지 여부에 따라 VC 변경
    - NotificationCenter를 통해 Reset 후 rootViewController를 변경하도록 구현

--- Tamagotchi/SceneDelegate.swift
+++ Tamagotchi/SceneDelegate.swift
@@ -8,10 +8,8 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -21,44 +19,64 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         
-        let homeNav = UINavigationController(rootViewController: HomeViewController())
-        window?.rootViewController = homeNav
-        window?.makeKeyAndVisible()
-    }
+        let isDone = UserDefaults.standard.bool(forKey: Keys.isDone)
+        
+        if isDone {
+            let rootVC = HomeViewController()
+            let navVC = UINavigationController(rootViewController: rootVC)
+            window?.rootViewController = navVC
+        } else {
+            let rootVC = ChangeTamagotchiViewController()
+            rootVC.title = Constants.UI.Title.selectTG
 
+            rootVC.didSelect = { [weak self] in
+                let homeNav = UINavigationController(rootViewController: HomeViewController())
+                self?.window?.rootViewController = homeNav
+                self?.window?.makeKeyAndVisible()
+            }
+            
+            let navVC = UINavigationController(rootViewController: rootVC)
+            window?.rootViewController = navVC
+        }
+        window?.makeKeyAndVisible()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleResetRequest), name: Notification.Name(Keys.didReset), object: nil)
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
+    
     func configureNavAppearance() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.shadowColor = .clear
         appearance.backgroundColor = .tgBlue
+        appearance.shadowColor = .lightGray
         
         UINavigationBar.appearance().tintColor = .tgCyan
         UINavigationBar.appearance().standardAppearance = appearance
@@ -70,6 +88,22 @@ extension SceneDelegate: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         let backBarButtonItem = UIBarButtonItem(title: Constants.UI.Title.setting, style: .plain, target: nil, action: nil)
         navigationController.topViewController?.navigationItem.backBarButtonItem = backBarButtonItem
+    }
+}
+
+extension SceneDelegate {
+    @objc private func handleResetRequest() {
+        let vc = ChangeTamagotchiViewController()
+        vc.title = Constants.UI.Title.selectTG
+        vc.didSelect = { [weak self] in
+            let homeNav = UINavigationController(rootViewController: HomeViewController())
+            self?.window?.rootViewController = homeNav
+            self?.window?.makeKeyAndVisible()
+        }
+        
+        let navVC = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navVC
+        window?.makeKeyAndVisible()
     }
 }
 
