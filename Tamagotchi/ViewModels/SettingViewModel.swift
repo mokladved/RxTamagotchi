70c15f75c067a2a24ef0d029767c1ae50d74c48d
moklad <dev.moklad@gmail.com>
2025-08-24 22:03:50 +0900

    feat: 설정 화면 ViewModel 구현
    - MVVM, INPUT/OUTPUT 패턴, RxSwift, RxCoCoa 사용하여 로직 구현

---
+++ Tamagotchi/ViewModels/SettingViewModel.swift
@@ -0,0 +1,76 @@
+//
+//  SettingViewModel.swift
+//  Tamagotchi
+//
+//  Created by Youngjun Kim on 8/24/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class SettingViewModel {
+    private let settingMenus: [SettingMenu] = [
+        SettingMenu(title: Constants.UI.Title.setName, imageName: Constants.UI.Title.pencil),
+        SettingMenu(title: Constants.UI.Title.changeTG, imageName: Constants.UI.Title.moon),
+        SettingMenu(title: Constants.UI.Title.reset, imageName: Constants.UI.Title.clockwise)
+    ]
+    
+    struct Input {
+        let cellSelected: ControlEvent<IndexPath>
+    }
+    
+    struct Output {
+        let settingOptions: Driver<[SettingMenu]>
+        let nameSettingTapped: PublishRelay<Void>
+        let changeTamagotchiTapped: PublishRelay<Void>
+        let resetTapped: PublishRelay<Void>
+    }
+    
+    private let disposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        let nameSettingTappedRelay = PublishRelay<Void>()
+        let changeTamagotchiTappedRelay = PublishRelay<Void>()
+        let resetTappedRelay = PublishRelay<Void>()
+        
+        let settingOptions = Observable.just(settingMenus)
+            .asDriver(onErrorJustReturn: [])
+        
+        let selectedRow = input.cellSelected.map { indexPath in
+            indexPath.row }
+        
+        selectedRow
+            .filter { row in
+                row == 0 }
+            .map { _ in () }
+            .bind(to: nameSettingTappedRelay)
+            .disposed(by: disposeBag)
+        
+        
+        selectedRow
+            .filter { row in
+                row == 1 }
+            .map { _ in () }
+            .bind(to: changeTamagotchiTappedRelay)
+            .disposed(by: disposeBag)
+        
+        selectedRow
+            .filter { row in
+                row == 2 }
+            .map { _ in () }
+            .bind(to: resetTappedRelay)
+            .disposed(by: disposeBag)
+        
+        return Output(
+            settingOptions: settingOptions,
+            nameSettingTapped: nameSettingTappedRelay,
+            changeTamagotchiTapped: changeTamagotchiTappedRelay,
+            resetTapped: resetTappedRelay
+        )
+    }
+    
+    func resetData() {
+        TamagotchiManager().reset()
+    }
+}
