db1adac252d1f669b3bf805ec89f9c4de60e4197
moklad <dev.moklad@gmail.com>
2025-08-24 22:08:32 +0900

    feat: 전체 리셋을 위한 reset 메소드 구현

--- Tamagotchi/Manager/TamagotchiManager.swift
+++ Tamagotchi/Manager/TamagotchiManager.swift
@@ -8,21 +8,28 @@
 import Foundation
 
 final class TamagotchiManager {
+    private let userDefaults = UserDefaults.standard
 
     private let riceKey = "riceCount"
     private let waterKey = "dropletCount"
     private let nicknameKey = "nickname"
 
     func save(_ tamagotchi: Tamagotchi) {
-        UserDefaults.standard.set(tamagotchi.riceCount, forKey: riceKey)
-        UserDefaults.standard.set(tamagotchi.dropletCount, forKey: waterKey)
-        UserDefaults.standard.set(tamagotchi.nickname, forKey: nicknameKey)
+        userDefaults.set(tamagotchi.riceCount, forKey: riceKey)
+        userDefaults.set(tamagotchi.dropletCount, forKey: waterKey)
+        userDefaults.set(tamagotchi.nickname, forKey: nicknameKey)
     }
     
     func load() -> Tamagotchi {
-        let rice = UserDefaults.standard.integer(forKey: riceKey)
-        let water = UserDefaults.standard.integer(forKey: waterKey)
-        let nickname = UserDefaults.standard.string(forKey: nicknameKey) ?? "대장"
+        let rice = userDefaults.integer(forKey: riceKey)
+        let water = userDefaults.integer(forKey: waterKey)
+        let nickname = userDefaults.string(forKey: nicknameKey) ?? "대장"
         return Tamagotchi(riceCount: rice, dropletCount: water, nickname: nickname)
+    }
+    
+    func reset() {
+        userDefaults.removeObject(forKey: nicknameKey)
+        userDefaults.removeObject(forKey: riceKey)
+        userDefaults.removeObject(forKey: waterKey)
     }
 }
