be2b0c3f9f5bc21204dad218d671e1cdb05579f6
moklad <dev.moklad@gmail.com>
2025-08-24 23:13:23 +0900

    feat: 매니저를 싱글톤으로 변경
    - BehaviorRelay를 사용하여 다마고치 상태를 전달.
    - 데이터 변경에 따라 UI가 업데이트 되도록 구현

--- Tamagotchi/Manager/TamagotchiManager.swift
+++ Tamagotchi/Manager/TamagotchiManager.swift
@@ -6,30 +6,48 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class TamagotchiManager {
+    
+    static let shared = TamagotchiManager()
+    
     private let userDefaults = UserDefaults.standard
 
     private let riceKey = "riceCount"
     private let waterKey = "dropletCount"
     private let nicknameKey = "nickname"
+    
+    let currentTamagotchi: BehaviorRelay<Tamagotchi>
+    
+    private init() {
+        let rice = userDefaults.integer(forKey: riceKey)
+        let water = userDefaults.integer(forKey: waterKey)
+        let nickname = userDefaults.string(forKey: nicknameKey) ?? "대장"
+        let initialValue = Tamagotchi(riceCount: rice, dropletCount: water, nickname: nickname)
+        
+        self.currentTamagotchi = BehaviorRelay(value: initialValue)
+        }
 
     func save(_ tamagotchi: Tamagotchi) {
         userDefaults.set(tamagotchi.riceCount, forKey: riceKey)
         userDefaults.set(tamagotchi.dropletCount, forKey: waterKey)
         userDefaults.set(tamagotchi.nickname, forKey: nicknameKey)
+        
+        currentTamagotchi.accept(tamagotchi)
     }
     
     func load() -> Tamagotchi {
-        let rice = userDefaults.integer(forKey: riceKey)
-        let water = userDefaults.integer(forKey: waterKey)
-        let nickname = userDefaults.string(forKey: nicknameKey) ?? "대장"
-        return Tamagotchi(riceCount: rice, dropletCount: water, nickname: nickname)
+        return currentTamagotchi.value
     }
     
     func reset() {
         userDefaults.removeObject(forKey: nicknameKey)
         userDefaults.removeObject(forKey: riceKey)
         userDefaults.removeObject(forKey: waterKey)
+        
+        let initialValue = Tamagotchi(riceCount: 0, dropletCount: 0, nickname: "대장")
+        currentTamagotchi.accept(initialValue)
     }
 }
