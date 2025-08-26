857b4a63e02cfaacb02153bef75ac47ff5183a90
moklad <dev.moklad@gmail.com>
2025-08-27 05:39:29 +0900

    fix: LocalizedError 프로토콜 추가 및 계산 프로퍼티명과 에러메시지 변경

--- Tamagotchi/Enums/Errors.swift
+++ Tamagotchi/Enums/Errors.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-enum LottoError: Error {
+enum LottoError: Error, LocalizedError {
     case invalid
     case networkDisconnected
     case apiError(Error)
     case unknown
     
-    var description: String? {
+    var errorDescription: String? {
         switch self {
         case .networkDisconnected:
             return "네트워크 연결이 끊겼습니다. "
         case .invalid:
             return "잘못된 회차 정보입니다."
         case .apiError(let error):
-            return "API 통신에 문제가 생겼습니다. \(error.localizedDescription)"
+            return "API 통신에 문제가 생겼습니다.\n\(error.localizedDescription)"
         case .unknown:
             return "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
         }
@@ -39,8 +39,8 @@ enum BoxOfficeError: Error, LocalizedError {
             return "네트워크 연결이 끊겼습니다."
         case .invalidFormat:
             return "날짜 형식이 올바르지 않습니다. yyyymmdd 형태로 입력해 주세요"
-        case .apiError:
-            return "통신 오류가 발생했습니다."
+        case .apiError(let error):
+            return "통신 오류가 발생했습니다.\n\(error.localizedDescription)"
         case .unknown:
             return "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해주세요."
         }
